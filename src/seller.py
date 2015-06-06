52e5089e4019a2c4eeb80c8cf2dabc5a04e213ef
unknown <b00902073@ntu.edu.tw>
2015-06-07 00:38:10 +0800

    add seller

---
+++ src/seller.py
@@ -0,0 +1,28 @@
+import csv
+
+class Seller(object):
+	def __init__(self, _id, store_name, address, longitude, latitude):
+		self.id = int(_id)
+		self.store_name = store_name if store_name.find("-") == -1 else store_name[:store_name.find("-")]
+		self.brance_name = "" if store_name.find("-") == -1 else store_name[store_name.find("-")+1:]
+		self.address = address
+		self.longitude = longitude
+		self.latitude = latitude
+	def _print(self):
+		print(self.id, self.store_name, self.brance_name, self.address, self.longitude, self.latitude)
+
+def list_sellers(csv_file):
+	#key is the ID
+	sellers = {}
+	with open(csv_file, 'r', errors='ignore') as f:
+		for row in csv.reader(f):
+			row = [s.replace("\n", "") for s in row]
+			sellers[int(row[0])] = Seller(*row)
+	return sellers
+		
+if __name__ == '__main__':
+	#TEST
+	sellers = list_sellers("../Taipei_shops_with_einvoice.csv")
+	#for key in sellers:
+	for i in range(1, 2000):
+		sellers[i]._print()
